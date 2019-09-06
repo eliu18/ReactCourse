e07d22aef25a96061ae02947461b3cb08e6f20f0
Eliu Benitez <eliu.benitez18@gmail.com>
2019-09-05 23:26:55 -0500

    Mapping and filtering components.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../components/Main.css";
 import GeometricShapes from "../components/GeometricShapes";
+import GeometricShapesData from "../shapes";
+
 function Main() {
+
+    const GeometricShapesObj = GeometricShapesData.map((shapeInfo, i) => <GeometricShapes key={i} shape={shapeInfo.shape} color={shapeInfo.color} />)
+    const GeometricShapesObjFiltered = GeometricShapesObj.filter((shapeInfo, i) => { if (shapeInfo.props.shape === "square") return <GeometricShapes key={i} shape={shapeInfo.props.shape} color={shapeInfo.props.color} /> })
     return (
         <div>
-            <GeometricShapes shape="square" color="blue"/>
-            <GeometricShapes shape="triangle" color="red"/>
-            <GeometricShapes shape="rectangle" color="yellow"/>
+            {GeometricShapesObjFiltered}
         </div>
     )
 }
---
+++ src/shapes.js
@@ -0,0 +1,20 @@
+const GeometricShapesData = [
+    {
+        shape: "rectangle",
+        color: "blue",
+    },
+    {
+        shape: "square",
+        color: "red",
+    },
+    {
+        shape: "triangle",
+        color: "green",
+    },
+    {
+        shape: "square",
+        color: "gray",
+    }
+]
+
+export default GeometricShapesData;
